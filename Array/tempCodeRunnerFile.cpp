98c78778c580b016777aed5f0d62d6d59f397d31
Priya Raj <priyati162505@gmail.com>
2026-06-12 19:04:55 +0530

    print subarray

--- Array/tempCodeRunnerFile.cpp
+++ Array/tempCodeRunnerFile.cpp
@@ -1,18 +1,4 @@
-#include <iostream>
-
-using namespace std;
-
-int main(){
-    int n = 5;
+int n = 5;
     int arr[5]={1,2,3,4,5};
-    for(int st=0;st<n;st++){
-        for(int end=st;end<n;end++){
-            for(int i = st;i<end;i++){
-                cout<<arr[i];
-            }
-            cout<<" ";
-        }
-        cout<<endl;
-    }
-    return 0;
-}
+    for(int st=0;st<n;st++){//start index 0 to 4
+        for(int end=st;end<n;end++)
