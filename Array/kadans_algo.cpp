962a160656d07167b6200fe91fab045b34baec99
Priya Raj <priyati162505@gmail.com>
2026-06-12 19:39:04 +0530

    kadans aldo

--- Array/kadans_algo.cpp
+++ Array/kadans_algo.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main(){
     int n = 6;
     int arr[6]={-1,-2,-3,4,5,8};
+    int currSum = 0;
     int maxSum=INT16_MIN;
-    for(int st=0;st<n;st++){
-      int  currSum = 0;
-        for(int end=st;end<n;end++){
-          currSum += arr[end];
+    for(int i = 0;i<n;i++){
+   
+          currSum += arr[i];
           maxSum = max(currSum,maxSum);
           if(currSum<0){
             currSum = 0;
           }
         }
        
-    }
+    
     cout<<"Max subArray sum "<<maxSum<<endl;
     return 0;
 }
