958f206b5559b363cbe5d72509304a464c39b955
Priya Raj <priyati162505@gmail.com>
2026-06-22 17:13:52 +0530

    binary search

---
+++ Algorithm/binary_search.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int binarySearch(vector<int> num,int target){
+    int n = num.size();
+    int st = 0,end = n-1;
+    while(st<=end){
+     int   mid=(st+end)/2;
+     if(target>num[mid]){
+        st = mid+1;
+     }
+     else if (target<num[mid])
+     {
+        end = mid - 1;
+     }
+     else{
+        return mid;
+     }
+     
+    }
+    return -1;
+} 
+int main(){
+    vector<int> num ={-1,0,3,4,5,9,12};
+    int target = 12;
+    cout<<binarySearch(num,target);
+    return 0;
+}
