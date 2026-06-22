1585a9e8171a10f2a49f2ecfe319c3b44730e6e6
Priya Raj <priyati162505@gmail.com>
2026-06-22 17:40:48 +0530

    binary search

--- Algorithm/binary_search.cpp
+++ Algorithm/binary_search.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+//binary search always apply on sorted array
 int binarySearch(vector<int> num,int target){
     int n = num.size();
     int st = 0,end = n-1;
     while(st<=end){
-     int   mid=(st+end)/2;
+     //int   mid=(st+end)/2;
+     int mid = st + (end - st)/2;//for overflow condition
+
      if(target>num[mid]){
         st = mid+1;
      }
@@ -21,8 +24,14 @@ int binarySearch(vector<int> num,int target){
     return -1;
 } 
 int main(){
-    vector<int> num ={-1,0,3,4,5,9,12};
-    int target = 12;
-    cout<<binarySearch(num,target);
+    //odd term
+    vector<int> num1 ={-1,0,3,4,5,9,12};
+    int target1 = 12;
+    cout<<binarySearch(num1,target1)<<endl;
+    //even term
+    vector<int> num2 ={-1,0,3,5,9,12};
+    int target2= 0;
+    cout<<binarySearch(num2,target2)<<endl;
+    
     return 0;
 }
