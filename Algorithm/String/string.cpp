847d6b4a62781a7f650e3b4a35109acf46b10acd
Priya Raj <priyati162505@gmail.com>
2026-07-04 21:48:44 +0530

    string

---
+++ Algorithm/String/string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+ string  str1 = "priya";
+ string  str2 = "raj";
+ string str3 = str1 + str2;//concatenation
+cout<<str3<<endl;
+cout<<(str1==str2)<<endl;
+cout<<(str1<str2)<<endl;
+cout<<str1.length()<<endl;
+
+    return 0;
+}
