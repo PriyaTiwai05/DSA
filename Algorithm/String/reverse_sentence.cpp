30d67349e86c3af9a7aaeb7360e4cf24cfcad26c
Priya Raj <priyati162505@gmail.com>
2026-08-28 01:42:11 +0530

    reverse word

---
+++ Algorithm/String/reverse_sentence.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+string reverseWord(string s){
+    int n = s.length();
+    string ans = "";
+    reverse(s.begin(),s.end());
+    for(int i=0;i<n;i++){
+       string word=" ";
+       while(i < n && s[i]!= ' '){
+word += s[i];
+i++;
+       }
+       reverse(word.begin(),word.end());
+       if(word.length()>0){
+        ans += " "+word;
+       }
+
+    }
+return ans.substr(1);
+}
+
+int main(){
+ string s;
+ cout<<"enter a string ";
+ getline(cin , s);
+ cout<<"output: "<<
+ reverseWord(s)<<endl;
+
+    return 0;
+}
