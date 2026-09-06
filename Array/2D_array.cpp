5fdfec8efca6fc782a50105d5d6dcba64f8eadbe
Priya Raj <priyati162505@gmail.com>
2026-09-06 14:35:47 +0530

    print 2d array

--- Array/2D_array.cpp
+++ Array/2D_array.cpp
@@ -6,6 +6,12 @@ int main(){
     int matrix[4][3] = {{1,2,3},{4,5,6,},{7,8,9},{10,11,12}};
     int row = 4;
     int coloum=3;
-    cout<<matrix[2][1];
+    for(int i = 0;i<row;i++){
+        for(int j = 0;j<coloum;j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
     return 0;
 }
